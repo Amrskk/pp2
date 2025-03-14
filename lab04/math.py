88821a3a7542db6dca4af5f4b6d0edff8a461a74
Amirlan <amirlanabd2@gmail.com>
2025-03-15 04:09:00 +0500

    d

--- lab04/math.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def degrees_to_radians(degrees):
-    return degrees * (math.pi / 180)
-
-degrees = int(input("Input degree: "))
-radians = degrees_to_radians(degrees)
-print(f"Output radian: {radians}")
