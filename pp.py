14d4eaa4f8551cffc1efe91e912992760811b122
Amirlan <amirlanabd2@gmail.com>
2025-03-30 11:42:32 +0500

    key

--- pp.py
+++ pp.py
@@ -1,4 +1,8 @@
-def hello(name):
-    name = input("Enter")
-    print(f"Hello {name}!")
-hello("name")
+def even(n):
+    i = 0
+    while i < n:
+        if i % 2 == 0:
+            print(i)
+            i+=1
+m = int(input())
+even(m)
